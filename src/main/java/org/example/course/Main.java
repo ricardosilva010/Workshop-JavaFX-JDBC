2b4565ee53f562dc50f71635f01fdac66953ccae
Ricardo Silva <p.ricardo.silva10@gmail.com>
2024-02-08 14:20:12 +0000

    Main view

--- src/main/java/org/example/course/Main.java
+++ src/main/java/org/example/course/Main.java
@@ -2,6 +2,7 @@ package org.example.course;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -12,9 +13,11 @@ public class Main extends Application
     @Override
     public void start(Stage stage) throws IOException
     {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
+        Parent parent = fxmlLoader.load();
+
+        Scene scene = new Scene(parent);
+        stage.setTitle("Sample JavaFX Application!");
         stage.setScene(scene);
         stage.show();
     }
