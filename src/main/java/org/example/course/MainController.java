270f57a779a94da8ed445599aedb5b4fa3c6e513
Ricardo Silva <p.ricardo.silva10@gmail.com>
2024-02-08 13:45:05 +0000

    Local project created

---
+++ src/main/java/org/example/course/MainController.java
@@ -0,0 +1,16 @@
+package org.example.course;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController
+{
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick()
+    {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
