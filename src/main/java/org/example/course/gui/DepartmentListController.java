512306359e47563440d129c3e8d762aabebe445d
Ricardo Silva <p.ricardo.silva10@gmail.com>
2024-02-10 19:45:05 +0000

    DepartmentForm (dialog) design

--- src/main/java/org/example/course/gui/DepartmentListController.java
+++ src/main/java/org/example/course/gui/DepartmentListController.java
@@ -2,17 +2,26 @@ package org.example.course.gui;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.example.course.Main;
+import org.example.course.gui.utils.Alerts;
+import org.example.course.gui.utils.Utils;
 import org.example.course.model.entities.Department;
 import org.example.course.model.services.DepartmentService;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -32,9 +41,10 @@ public class DepartmentListController implements Initializable
     private Button btNew;
 
     @FXML
-    public void onBtNewAction()
+    public void onBtNewAction(ActionEvent event)
     {
-        System.out.println("onBtNewAction");
+        Stage parentStage = Utils.currentStage(event);
+        createDialogForm("/org/example/course/DepartmentForm.fxml", parentStage);
     }
 
     public void setDepartmentService(DepartmentService departmentService)
@@ -66,5 +76,26 @@ public class DepartmentListController implements Initializable
         List<Department> list = departmentService.findAll();
         observableList = FXCollections.observableArrayList(list);
         tableViewDepartment.setItems(observableList);
+    }
+
+    private void createDialogForm(String absoluteName, Stage parentStage)
+    {
+        try
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(absoluteName));
+            Pane pane = fxmlLoader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Enter Department data");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.showAndWait();
+        }
+        catch (IOException e)
+        {
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
